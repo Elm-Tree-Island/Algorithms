23a04c99799be695ae1255eddf4a4abd5e0e6c1e
Mark Chen <chenjian345@gmail.com>
2019-12-10 18:53:08 +0800

    Add the 'Best time to buy and sell stock' problem, the solution is not finished

--- leetcode/source/maxSubArray.c
+++ leetcode/source/maxSubArray.c
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 
+// 53. Maximum Subarray
+// https://leetcode.com/problems/maximum-subarray/
 int maxSubArray(int* nums, int numsSize) {
     if (numsSize <= 0) {
         return 0;
@@ -27,3 +29,38 @@ int maxSubArray(int* nums, int numsSize) {
     
     return max;
 }
+
+// 121. Best Time to Buy and Sell Stock
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+int maxProfit(int* prices, int pricesSize){
+    if (pricesSize <= 0) {
+        return 0;
+    }
+    
+    /** Brute Force **/
+    //     int max_profit = 0;
+    //     int buy = 0;
+    //     int sell = 0;
+    //     for (int i = 0; i<pricesSize; i++) {
+    //         for (int j = i+1; j<pricesSize; j++) {
+    //             int profit = prices[j] - prices[i];
+    //             if (profit > max_profit) {
+    //                 max_profit = profit;
+    //                 buy = i;
+    //                 sell = j;
+    //             }
+    //         }
+    //     }
+    
+    //     printf("Buy at %d, Sell at %d", prices[buy], prices[sell]);
+    //     return max_profit;
+    
+
+    /** Other Way **/
+    int max_profit = 0;
+    
+}
+
+
+
+
